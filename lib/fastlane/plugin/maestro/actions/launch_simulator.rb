3d0609093c6f7dccf6601e9c80921b4ac1ee818c
Marc Bormeth <marc.bormeth@sap.com>
2024-12-13 10:30:07 +0100

    (Re)install app if reusing iOS simulator

--- lib/fastlane/plugin/maestro/actions/launch_simulator.rb
+++ lib/fastlane/plugin/maestro/actions/launch_simulator.rb
@@ -26,12 +26,16 @@ module Fastlane
           if existing_device.state == :booted
             UI.message("Device #{device_name} is already running. Patching settings…")
             patch_device_settings(existing_device, params)
+            UI.message("Installing app #{params[:app_path]} on the simulator")
+            device.install(params[:app_path])
             return existing_device
           elsif existing_device.state == :shutdown
             UI.message("Device #{device_name} is shutdown. Booting device…")
             existing_device.boot
             existing_device.wait { |d| d.state == :booted }
             patch_device_settings(existing_device, params)
+            UI.message("Installing app #{params[:app_path]} on the simulator")
+            device.install(params[:app_path])
             return existing_device
           end
         end
